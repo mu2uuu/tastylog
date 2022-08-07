b776c67e588a371331a86451aba7d79aca1ad3d5
p-nuts5963 <polythene.pam5963@gimail.com>
2022-08-07 15:58:19 +0900

    セクション15

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const app = express();
 app.set("view engine", "ejs");
 app.disable("x-powered-by");
 
+// Expose global method to view engine
+app.use((req, res, next) => {
+  res.locals.moment = require("moment");
+  res.locals.padding = require("./lib/math/math.js").padding;
+  next();
+});
+
 // Static resource rooting
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
@@ -19,19 +26,8 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(accesslogger());
 
 // Dynamic resource rooting
+app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
-app.use("/test", async (req, res, next) => {
-  const { MySQLClient, sql } = require("./lib/database/client");
-  var data;
-
-  try {
-    data = await MySQLClient.executeQuery(await sql("SELECT_SHOP_BASIC_BY_ID"), [2]);
-    console.log(data);
-  } catch (err) {
-    next(err);
-  }
-  res.end("OK");
-});
 
 // Set application log
 app.use(applicationlogger());
