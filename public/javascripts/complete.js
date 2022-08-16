674d73b215eb5ec087ed5ee273e0cb5e57976ea2
p-nuts5963 <polythene.pam5963@gimail.com>
2022-08-16 23:32:29 +0900

    セクション18

---
+++ public/javascripts/complete.js
@@ -0,0 +1,9 @@
+var window_onpopstate = function (event) {
+  history.pushState(null, null, null);
+};
+var document_onready = function (event) {
+  history.pushState(null, null, null);
+  $(window).on("popstate", window_onpopstate);
+};
+
+$(document).ready(document_onready);
